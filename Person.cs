874fe72fd8586e4bb5878c530b54d61eb585a879
Vishwa1017 <97929298+Vishwa1017@users.noreply.github.com>
2023-05-09 05:49:01 +0530

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+
+        private int salary;
+        public string FullName;
+
+
+        public void setSalary()
+        {
+            salary = 33000;
+        }
+
+        public int getSalary()
+        {
+            return salary;
+        }
+
+        public string getFullName()
+        {
+            FullName = Name + FirstName;
+            return FullName;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HelloWorld;
+
+Console.WriteLine("Hello, World!");
+
+var name = "Vishwa Raman";
+
+Console.WriteLine("Creative learner " + name);
+
+string na;
+Console.WriteLine("Enter your name");
+na = Console.ReadLine();
+Console.WriteLine("Entered Name" + na);
+Console.WriteLine("Enter Name {0}", na);
+Console.WriteLine($"Entered Name {na} cool");
+
+
+
+DateTime td = DateTime.Now;
+
+Console.WriteLine($"Time {td}");
+
+Person p = new Person();
+p.Name = Console.ReadLine();
+p.FirstName = Console.ReadLine();
+p.Age = Convert.ToInt32(Console.ReadLine());
+p.setSalary();
+
+Console.WriteLine(p.Name);
+Console.WriteLine(p.FirstName);
+Console.WriteLine(p.Age);
+Console.WriteLine("Salary is "+ p.getSalary());
+Console.WriteLine("Full Name" + p.getFullName());
